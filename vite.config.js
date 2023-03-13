2c9d8f2292181c9931978a03277e551f8b4f83a3
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-03-13 11:27:40 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import autoprefixer from 'autoprefixer';
+
+export default defineConfig({
+    server: {
+        open: true,
+        host: true
+    },
+
+    base: "",
+    build: {
+        outDir: "./docs"
+    },
+
+    css: {
+        postcss: {
+            plugins: [
+                autoprefixer()
+            ],
+        }
+    }
+})
